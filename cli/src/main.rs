6ce149a911df16ff58518f5d213a22063f7d840c
chancehudson <jchancehud@gmail.com>
2025-06-29 17:42:15 -0500

    feat(cli): better errors

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,6 +9,7 @@ use clap::ArgAction;
 use clap::Command;
 use nanoid::nanoid;
 use onyx_api::prelude::*;
+use std::fs;
 use tempfile::tempfile;
 use tokio;
 
@@ -16,6 +17,21 @@ mod publish;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    if let Err(err) = run().await {
+        eprintln!("❌ {}", err);
+
+        // Print all errors in the chain
+        for (i, cause) in err.chain().enumerate().skip(1) {
+            eprintln!("  {}: {}", i, cause);
+        }
+
+        std::process::exit(1);
+    } else {
+        Ok(())
+    }
+}
+
+async fn run() -> Result<()> {
     let matches = cli().get_matches();
     let api = OnyxApi::default();
     // let login = LoginResponse { user: (), token: (), expires_at: () }
@@ -33,6 +49,13 @@ async fn main() -> Result<()> {
             })
             .unwrap_or(cwd);
         println!("📦 Packaging {:?}", path);
+        if let Ok(metadata) = fs::metadata(&path) {
+            if !metadata.is_dir() {
+                anyhow::bail!("Path is not a directory: {:?}", path);
+            }
+        } else {
+            anyhow::bail!("Unable to stat path: {:?}", path);
+        }
         let mut tarball = nrpm_tarball::create(path, tempfile()?)?;
         if let Some(archive_path) = matches.get_one::<String>("archive") {
             io::copy(&mut tarball, &mut File::create(archive_path)?)?;
